5dd44e1722b147ab0fda5bf2a5ff2c51523a7e69
Izzy <isaiah.ozuna@yahoo.com>
2022-04-19 12:05:53 -0500

    prevented submitting an empty form

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,6 +5,12 @@ var taskFormHandler = function (event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  //check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  formEl.reset();
 
   //package up data as an object
   var taskDataObj = {
@@ -20,6 +26,7 @@ var createTaskEl = function (taskDataObj) {
   //create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
+
   //create div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
   //give it a class name
